978593011695549168cfd99063525294a7bfc609
Dpepe <goete_df9@hotmail.com>
2024-02-05 12:42:10 -0600

    first

---
+++ precache-manifest.c33eb56d61ac35c468ac25e630692134.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856404adb6223f525db5f2064c6858a9",
+    "url": "/Dpepe/crud-react/index.html"
+  },
+  {
+    "revision": "96b6ee9d2a93daf1551f",
+    "url": "/Dpepe/crud-react/static/css/2.847db71a.chunk.css"
+  },
+  {
+    "revision": "dd024d7869935bfebb98",
+    "url": "/Dpepe/crud-react/static/css/main.70fd5fe0.chunk.css"
+  },
+  {
+    "revision": "96b6ee9d2a93daf1551f",
+    "url": "/Dpepe/crud-react/static/js/2.ffc1ce06.chunk.js"
+  },
+  {
+    "revision": "b17489c7bc469614970e635453698a8d",
+    "url": "/Dpepe/crud-react/static/js/2.ffc1ce06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd024d7869935bfebb98",
+    "url": "/Dpepe/crud-react/static/js/main.227244e3.chunk.js"
+  },
+  {
+    "revision": "ed68548d0411e33d8905",
+    "url": "/Dpepe/crud-react/static/js/runtime-main.3dc2c6d1.js"
+  },
+  {
+    "revision": "4000efdcc3fa61020a69d63a05d05096",
+    "url": "/Dpepe/crud-react/static/media/ankora.4000efdc.jpg"
+  },
+  {
+    "revision": "cd04835002514f8fd00ab2d9e617f738",
+    "url": "/Dpepe/crud-react/static/media/creador-pdf-departamentos-bosque.cd048350.JPG"
+  },
+  {
+    "revision": "460cf4239b64aca09b6e76a638c94c0a",
+    "url": "/Dpepe/crud-react/static/media/creador-pdf-departamentos-nova.460cf423.JPG"
+  },
+  {
+    "revision": "2ef9fdc7850feb704139926f6e518379",
+    "url": "/Dpepe/crud-react/static/media/creador-pdf-departamentos.2ef9fdc7.JPG"
+  },
+  {
+    "revision": "130f8e72ad96dc3ac83f3ecd8584b97c",
+    "url": "/Dpepe/crud-react/static/media/dcid.130f8e72.jpg"
+  },
+  {
+    "revision": "7e5d5c89a30fe506ba83b4cc8075c7b4",
+    "url": "/Dpepe/crud-react/static/media/fondo-body.7e5d5c89.jpg"
+  },
+  {
+    "revision": "d86860d5063653b7dca538ecd36fae9d",
+    "url": "/Dpepe/crud-react/static/media/fondo-perfil-azul.d86860d5.png"
+  },
+  {
+    "revision": "119c3267d96c2154b08757b50fd749d6",
+    "url": "/Dpepe/crud-react/static/media/fondo-perfil-html.119c3267.jpg"
+  },
+  {
+    "revision": "f71b797c7aa09f388faf2ed0af6ffb3b",
+    "url": "/Dpepe/crud-react/static/media/fondo-perfil-verdel.f71b797c.png"
+  },
+  {
+    "revision": "75545ecd939a2b146efd8754637ee24d",
+    "url": "/Dpepe/crud-react/static/media/fondotren-blur.75545ecd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dpepe/crud-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ae82e97d40469631c564a8029e161ab",
+    "url": "/Dpepe/crud-react/static/media/pantallasa.2ae82e97.jpg"
+  },
+  {
+    "revision": "45a30609cc93cf39c791bcb9045c50e2",
+    "url": "/Dpepe/crud-react/static/media/verchitos.45a30609.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Dpepe/crud-react/precache-manifest.c33eb56d61ac35c468ac25e630692134.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dpepe/crud-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
